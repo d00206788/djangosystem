c351c6d80a522b260bbbd3dbbc86214eaedd8310
Katie Rice <75254760+d00206788@users.noreply.github.com>
2023-12-18 16:58:01 +0000

    Update tests.py

--- djangomainproject/djangoapp/tests.py
+++ djangomainproject/djangoapp/tests.py
@@ -1,3 +1,41 @@
 from django.test import TestCase
+from djangoapp.forms import *
 
 # Create your tests here.
+
+class MyTests(TestCase):
+    def test_forms(self):
+        form_data = {'password1': 'password1'}
+        form = CreateUserForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+class MyTests(TestCase):
+    def test_forms(self):
+        form_data = {'password2': 'password2'}
+        form = CreateUserForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+class MyTests(TestCase):
+    def test_forms(self):
+        form_data = {'email': 'email'}
+        form = CreateUserForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+class MyTests(TestCase):
+    def test_forms(self):
+        form_data = {'username': 'username'}
+        form = CreateUserForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+
+class MyTests(TestCase):
+    def test_forms(self):
+        form_data = {'username': 'username'}
+        form = UpdateUserForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+class MyTests(TestCase):
+    def test_forms(self):
+        form_data = {'email': 'email'}
+        form = UpdateUserForm(data=form_data)
+        self.assertTrue(form.is_valid())
