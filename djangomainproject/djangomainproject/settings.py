c4ec0091c0944542ea1b92aa14df9056cec42ea7
Katie Rice <75254760+d00206788@users.noreply.github.com>
2024-03-02 23:00:48 +0000

    Update settings.py

--- djangomainproject/djangomainproject/settings.py
+++ djangomainproject/djangomainproject/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = 'django-insecure-wl0g9$p1ff-)m7wxj69h%7cu9)a_g5@eri!^(_#^1o0q01xenq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['djangomainproject.onrender.com', '127.0.0.1', '5432',]
+ALLOWED_HOSTS = ['https://djangomainproject.onrender.com
+
+', '127.0.0.1', '5432',]
 
 
 # Application definition
